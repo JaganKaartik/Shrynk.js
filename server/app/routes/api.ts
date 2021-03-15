0133a393c12ae7f7702f70d871789956b3605084
JaganKaartik <kaartikjagan@gmail.com>
2021-03-15 23:59:43 +0530

    Commented Other API Routes

--- server/app/routes/api.ts
+++ server/app/routes/api.ts
@@ -10,8 +10,8 @@ import SessionCheck from '../middleware/SessionCheck'
 const apiRouter = express.Router()
 
 apiRouter.get('/get', SessionCheck, getController)
-apiRouter.post('/post', SessionCheck, postController)
-apiRouter.delete('/delete', SessionCheck, deleteController)
-apiRouter.put('/put', SessionCheck, putController)
+// apiRouter.post('/post', SessionCheck, postController)
+// apiRouter.delete('/delete', SessionCheck, deleteController)
+// apiRouter.put('/put', SessionCheck, putController)
 
 export = apiRouter
