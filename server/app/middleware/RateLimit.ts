c4cbb4d823e3f31b5917f91b440da5f04ce9d28b
JaganKaartik <kaartikjagan@gmail.com>
2021-03-16 22:46:18 +0530

    Updated APIs

--- server/app/middleware/RateLimit.ts
+++ server/app/middleware/RateLimit.ts
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit')
 
 const apiLimiter = rateLimit({
   windowMs: 1 * 60 * 1000,
-  max: 25,
+  max: 10,
   message:
     'Too many accounts created from this IP, please try again after a minute'
 })
