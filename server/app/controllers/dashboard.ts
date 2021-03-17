b6e9114d803572c94a2fbd374693c45dfd1779a2
JaganKaartik <kaartikjagan@gmail.com>
2021-03-17 23:43:17 +0530

    Added Dashboard Controller

--- server/app/controllers/dashboard.ts
+++ server/app/controllers/dashboard.ts
@@ -1,3 +1,15 @@
-const getAllURLS = async (id) => {}
+const URLS = require('../models/URL')
+
+const getAllURLS = async (req, res) => {
+  URLS.findOne({ userId: req.user.userId })
+    .then((data: JSON) => {
+      res.send(data)
+    })
+    .catch((err) => {
+      res.status(404).json({
+        message: 'Record does not Exist'
+      })
+    })
+}
 
 export = getAllURLS
