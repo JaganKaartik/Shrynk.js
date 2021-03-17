a729bc3204ebdab5639562109e63d0281851efd7
JaganKaartik <kaartikjagan@gmail.com>
2021-03-17 15:01:23 +0530

    Added Validation -- URL

--- server/app/controllers/api.ts
+++ server/app/controllers/api.ts
@@ -1,29 +1,34 @@
 import { IResult } from '../interface'
 
 const URLS = require('../models/URL')
-const { generateId, urlCheck } = require('../services/URLServices')
+const { generateID, validID, urlCheck } = require('../services/URLServices')
 const { CLIENT_ORIGIN } = require('../config/default.config')
 
 export * from '../interface'
 
 const shortenURL = async (req, res) => {
-  const id = generateId()
-  const checkedId = await urlCheck(id)
-  URLS.create({
-    urlCode: checkedId,
-    longURL: req.body.longURL,
-    shortURL: `${CLIENT_ORIGIN}/${id}`
-  })
-    .then((resp: JSON) => {
-      if (resp) {
-        res.send({ success: true })
-      } else {
-        res.send({ success: false })
-      }
+  const id = generateID()
+  const checkedId = await validID(id)
+  const urlCheckResp = await urlCheck(req.body.longURL)
+  if (urlCheckResp) {
+    URLS.create({
+      urlCode: checkedId,
+      longURL: req.body.longURL,
+      shortURL: `${CLIENT_ORIGIN}/${id}`
     })
-    .catch((err) => {
-      res.send(err)
-    })
+      .then((resp: JSON) => {
+        if (resp) {
+          res.send({ success: true })
+        } else {
+          res.send({ success: false })
+        }
+      })
+      .catch((err) => {
+        res.send(err)
+      })
+  } else {
+    res.send('URL Not Valid')
+  }
 }
 
 const redirectToURL = (req, res) => {
--- server/app/services/URLServices.ts
+++ server/app/services/URLServices.ts
@@ -1,15 +1,22 @@
 const { nanoid } = require('nanoid')
+const isUrl = require('is-valid-http-url')
+
 const URLS = require('../models/URL')
 
-export const generateId = () => {
+export const generateID = () => {
   const id = nanoid(10)
   return id
 }
 
-export const urlCheck = async (id) => {
+export const validID = async (id) => {
   const result = await URLS.findOne({ urlCode: id }).then((res) => {
-    const resp = !res ? id : generateId()
+    const resp = !res ? id : generateID()
     return resp
   })
   return result
+}
+
+export const urlCheck = async (url) => {
+  const resp = await isUrl(url)
+  return resp
 }
