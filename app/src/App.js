baac5b4c93a57503dc23446ab58da6ef99a13218
JaganKaartik <kaartikjagan@gmail.com>
2021-03-18 17:09:39 +0530

    Added Tailwind css

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Button } from "antd";
-import "./App.css";
+import "./styles/App.css";
+import logo from "./images/bg2.gif";
 
 const App = () => (
   <div className="App">
-    <Button type="primary">Button</Button>
+    <img src={logo} alt="loading..." />
   </div>
 );
 
