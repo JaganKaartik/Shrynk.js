3628ff4d370cecd3d0234b977f64796f9b21e556
JaganKaartik <kaartikjagan@gmail.com>
2021-03-19 16:04:49 +0530

    Updated JWT Check Token

--- server/app/middleware/jwt.ts
+++ server/app/middleware/jwt.ts
@@ -4,15 +4,27 @@ const jwt = require('jsonwebtoken')
 const { JWT_SECRET } = require('../config/default.config')
 
 export default (req, res, next) => {
-  const token = req.header('x-auth-token')
-  try {
-    const decoded = jwt.verify(token, JWT_SECRET)
-    req.user = decoded
-    next()
-  } catch (e) {
-    res.status(401).json({
-      authenticated: false,
-      message: 'token is not valid'
+  let token = req.headers['x-access-token'] || req.headers.authorization // Express headers are auto converted to lowercase
+  if (token.startsWith('Bearer ')) {
+    // Remove Bearer from string
+    token = token.slice(7, token.length)
+  }
+  if (token) {
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if (!err) {
+        req.decoded = decoded
+        next()
+      } else {
+        return res.json({
+          success: false,
+          message: 'Token is not valid'
+        })
+      }
+    })
+  } else {
+    return res.json({
+      success: false,
+      message: 'Auth token is not supplied'
     })
   }
 }
