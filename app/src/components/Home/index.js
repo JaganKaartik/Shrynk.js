52b9336049a8b977a1eb441fdb6a3b4f01451b95
JaganKaartik <kaartikjagan@gmail.com>
2021-04-18 20:31:32 +0530

    Minor. Updated UI

--- app/src/components/Home/index.js
+++ app/src/components/Home/index.js
@@ -58,12 +58,12 @@ export default function Home() {
           style={{ maxWidth: '500px', margin: '50px auto 0' }}
         />
       </div>
-
+      <br />
       <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
         <div className="md:flex">
           <div className="p-8">
             <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-              Case study
+              Why use Shrynk ?
             </div>
             <p className="mt-2 text-gray-500">
               Can’t remember a URL easily ? Can’t use the links where there are
@@ -89,28 +89,30 @@ export default function Home() {
               Features
             </div>
 
-            <ul className="p-1 list-disc">
-              <li>
-                <p className="font-sans md:font-serif block mt-1 text-lg leading-tight font-medium text-black">
-                  Tiers and Quotas on URLs.
-                </p>
-              </li>
-              <li>
-                <p className="font-sans md:font-serif block mt-1 text-lg leading-tight font-medium text-black">
-                  Onboarding Plans.
-                </p>
-              </li>
-              <li>
-                <p className="font-sans md:font-serif block mt-1 text-lg leading-tight font-medium text-black">
-                  OAuth Authentication
-                </p>
-              </li>
-              <li>
-                <p className="font-sans md:font-serif block mt-1 text-lg leading-tight font-medium text-black">
-                  Dashboard to CRUD URLs.
-                </p>
-              </li>
-            </ul>
+            <div>
+              <ul className="p-1">
+                <li>
+                  <p className="block mt-1 text-lg leading-tight font-mono text-black">
+                    Tiers and Quotas on URLs
+                  </p>
+                </li>
+                <li>
+                  <p className="block mt-1 text-lg leading-tight font-mono text-black">
+                    Onboarding
+                  </p>
+                </li>
+                <li>
+                  <p className="block mt-1 text-lg leading-tight font-mono text-black">
+                    OAuth Authentication
+                  </p>
+                </li>
+                <li>
+                  <p className="block mt-1 text-lg leading-tight font-mono text-black">
+                    Dashboard to CRUD generated URLs
+                  </p>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
@@ -118,7 +120,7 @@ export default function Home() {
       <br />
 
       <div className="flex justify-center">
-        <div className="md:flex-shrink-0 grid grid-cols-2 gap-4">
+        <div className="md:flex-shrink-0 grid grid-cols-1 gap-4">
           <button
             className="rounded-xl w-full shadow-2xl inline-flex items-center h-12 px-5 text-indigo-100 transition duration-200 ease-in-out bg-indigo-500 hover:bg-red-400 transform hover:-translate-y-1 hover:scale-110 .."
             onClick={() => onClickHandler('google')}
@@ -131,7 +133,7 @@ export default function Home() {
               <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm167 633.6C638.4 735 583 757 516.9 757c-95.7 0-178.5-54.9-218.8-134.9C281.5 589 272 551.6 272 512s9.5-77 26.1-110.1c40.3-80.1 123.1-135 218.8-135 66 0 121.4 24.3 163.9 63.8L610.6 401c-25.4-24.3-57.7-36.6-93.6-36.6-63.8 0-117.8 43.1-137.1 101-4.9 14.7-7.7 30.4-7.7 46.6s2.8 31.9 7.7 46.6c19.3 57.9 73.3 101 137 101 33 0 61-8.7 82.9-23.4 26-17.4 43.2-43.3 48.9-74H516.9v-94.8h230.7c2.9 16.1 4.4 32.8 4.4 50.1 0 74.7-26.7 137.4-73 180.1z" />
             </svg>
           </button>
-          <button
+          {/* <button
             className="rounded-xl w-full shadow-2xl inline-flex items-center h-12 px-5 text-indigo-100 transition duration-200 ease-in-out bg-indigo-500 hover:bg-red-400 transform hover:-translate-y-1 hover:scale-110 .."
             onClick={() => onClickHandler('twitter')}
           >
@@ -142,7 +144,7 @@ export default function Home() {
             >
               <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm167 633.6C638.4 735 583 757 516.9 757c-95.7 0-178.5-54.9-218.8-134.9C281.5 589 272 551.6 272 512s9.5-77 26.1-110.1c40.3-80.1 123.1-135 218.8-135 66 0 121.4 24.3 163.9 63.8L610.6 401c-25.4-24.3-57.7-36.6-93.6-36.6-63.8 0-117.8 43.1-137.1 101-4.9 14.7-7.7 30.4-7.7 46.6s2.8 31.9 7.7 46.6c19.3 57.9 73.3 101 137 101 33 0 61-8.7 82.9-23.4 26-17.4 43.2-43.3 48.9-74H516.9v-94.8h230.7c2.9 16.1 4.4 32.8 4.4 50.1 0 74.7-26.7 137.4-73 180.1z" />
             </svg>
-          </button>
+          </button> */}
           <br />
         </div>
       </div>
