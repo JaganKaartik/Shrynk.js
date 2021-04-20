fa8b3610f861e5a749e336938566a6ab7ecb1379
JaganKaartik <kaartikjagan@gmail.com>
2021-04-21 02:47:55 +0530

    Minor: Server Updates

--- server/app/middleware/SessionCheck.ts
+++ server/app/middleware/SessionCheck.ts
@@ -2,17 +2,16 @@
 
 const jwt = require('jsonwebtoken')
 const { JWT_SECRET } = require('../config/default.config')
-
+// eslint-disable-next-line consistent-return
 export default (req, res, next) => {
   let token = req.headers['x-access-token'] || req.headers.authorization
   if (token.startsWith('Bearer ')) {
     // Remove Bearer from string
     token = token.slice(7, token.length)
   }
-  console.log(token)
   if (token) {
+    // eslint-disable-next-line consistent-return
     jwt.verify(token, JWT_SECRET, (err, decoded) => {
-      console.log(decoded)
       // eslint-disable-next-line valid-typeof
       if (typeof err !== null) {
         req.decoded = decoded
