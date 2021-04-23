5c5c9cdf9bb52d89d1aa9f2f20d635aced16a01a
JaganKaartik <kaartikjagan@gmail.com>
2021-04-23 09:18:44 +0530

    Minor

--- server/app/controllers/analytics.ts
+++ server/app/controllers/analytics.ts
@@ -3,9 +3,8 @@ const Analytics = require('../models/Analytics')
 const getAnalyticsData = async (req, res) => {
   if (req.params.userid) {
     const result = await Analytics.find({ userId: req.params.userid })
-    if (result.length !== 0) {
-      res.send(result)
-    } else {
+    if (result.length !== 0) res.send(result)
+    else {
       res.status(422).send({ message: 'Insufficient Analytics Data present' })
     }
   } else {
