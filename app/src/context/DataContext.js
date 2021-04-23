5ad76d83acc81159e6fa8677c9eec64b72dc240a
JaganKaartik <kaartikjagan@gmail.com>
2021-04-24 01:42:54 +0530

    Refactored Data Context for Analytics Data

--- app/src/context/DataContext.js
+++ app/src/context/DataContext.js
@@ -5,11 +5,13 @@ const DataContext = createContext([[], () => {}]);
 const DataProvider = (props) => {
   const [data, setData] = useState('');
   const [update, didUpdate] = useState(false);
+  const [AnalyticsData, setAnalyticsData] = useState('');
   return (
     <DataContext.Provider
       value={{
         dataFetched: { data, setData },
         dataUpdated: { update, didUpdate },
+        dataAnalytics: { AnalyticsData, setAnalyticsData },
       }}
     >
       {props.children}
