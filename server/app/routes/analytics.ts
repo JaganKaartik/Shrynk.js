0d755b0f29c6a45e9c5e72bcd2c7c0efcc60026b
JaganKaartik <kaartikjagan@gmail.com>
2021-04-24 22:01:13 +0530

    Added Analytics Controllers/Routes

--- server/app/routes/analytics.ts
+++ server/app/routes/analytics.ts
@@ -1,9 +1,13 @@
 import express from 'express'
 import SessionCheck from '../middleware/SessionCheck'
-import getAnalyticsData from '../controllers/analytics'
+import {
+  getAnalyticsData,
+  getTotalVisitsForURL
+} from '../controllers/analytics'
 
 const analyticsRouter = express.Router()
 
 analyticsRouter.get('/:userid', SessionCheck, getAnalyticsData)
+analyticsRouter.get('/:userid/:urlcode', SessionCheck, getTotalVisitsForURL)
 
 export = analyticsRouter
