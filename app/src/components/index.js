a898e91826323b1f5b39197fb7eb9c4565b03cdc
JaganKaartik <kaartikjagan@gmail.com>
2021-04-26 05:29:12 +0530

    Updates

---
+++ app/src/components/index.js
@@ -0,0 +1,15 @@
+import Home from './Home';
+import Onboarding from './Onboarding';
+import Dashboard from './Dashboard';
+import Navbar from './Navbar';
+import AuthComponent from './Auth/AuthComponent';
+import AnalyticsDashboard from './Analytics';
+
+export {
+  Home,
+  Onboarding,
+  Dashboard,
+  Navbar,
+  AuthComponent,
+  AnalyticsDashboard,
+};
