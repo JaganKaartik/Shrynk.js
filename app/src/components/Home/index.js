08d654829682a6ac17197faaab30c645a1532049
JaganKaartik <kaartikjagan@gmail.com>
2021-05-02 21:51:05 +0530

    Added Github Auth -- Client Side

--- app/src/components/Home/index.js
+++ app/src/components/Home/index.js
@@ -62,7 +62,6 @@ export default function Home() {
         />
       </div>
       <br />
-
       <div className="custom-card max-w-md mx-auto rounded-xl shadow-2xl overflow-hidden md:max-w-2xl">
         <div className="sm:flex">
           <div className="p-8">
@@ -141,24 +140,40 @@ export default function Home() {
         </div>
       </div>
       <br />
-
       <div className="flex justify-center">
-        <div className="md:flex-shrink-0 grid grid-cols-1 gap-4">
-          <button
-            className="rounded-xl w-full shadow-2xl inline-flex items-center h-12 px-5 text-indigo-100 transition duration-200 ease-in-out bg-blue-600 hover:bg-red-400 transform hover:-translate-y-1 hover:scale-110 .."
-            onClick={() => onClickHandler('google')}
-          >
-            <span>Login with Google</span>
-            <svg
-              className="w-5 h-5 ml-2  fill-current icon"
-              viewBox="0 0 1024 1024"
+        <div className="md:flex-shrink-0 grid grid-cols-2 gap-4">
+          <div>
+            <button
+              className="rounded-xl w-full shadow-2xl inline-flex items-center h-12 px-5 text-indigo-100 transition duration-200 ease-in-out bg-blue-600 hover:bg-red-400 transform hover:-translate-y-1 hover:scale-110 .."
+              onClick={() => onClickHandler('google')}
             >
-              <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm167 633.6C638.4 735 583 757 516.9 757c-95.7 0-178.5-54.9-218.8-134.9C281.5 589 272 551.6 272 512s9.5-77 26.1-110.1c40.3-80.1 123.1-135 218.8-135 66 0 121.4 24.3 163.9 63.8L610.6 401c-25.4-24.3-57.7-36.6-93.6-36.6-63.8 0-117.8 43.1-137.1 101-4.9 14.7-7.7 30.4-7.7 46.6s2.8 31.9 7.7 46.6c19.3 57.9 73.3 101 137 101 33 0 61-8.7 82.9-23.4 26-17.4 43.2-43.3 48.9-74H516.9v-94.8h230.7c2.9 16.1 4.4 32.8 4.4 50.1 0 74.7-26.7 137.4-73 180.1z" />
-            </svg>
-          </button>
-          <br />
+              <span>Login with Google</span>
+              <svg
+                className="w-5 h-5 ml-2  fill-current icon"
+                viewBox="0 0 1024 1024"
+              >
+                <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm167 633.6C638.4 735 583 757 516.9 757c-95.7 0-178.5-54.9-218.8-134.9C281.5 589 272 551.6 272 512s9.5-77 26.1-110.1c40.3-80.1 123.1-135 218.8-135 66 0 121.4 24.3 163.9 63.8L610.6 401c-25.4-24.3-57.7-36.6-93.6-36.6-63.8 0-117.8 43.1-137.1 101-4.9 14.7-7.7 30.4-7.7 46.6s2.8 31.9 7.7 46.6c19.3 57.9 73.3 101 137 101 33 0 61-8.7 82.9-23.4 26-17.4 43.2-43.3 48.9-74H516.9v-94.8h230.7c2.9 16.1 4.4 32.8 4.4 50.1 0 74.7-26.7 137.4-73 180.1z" />
+              </svg>
+            </button>
+          </div>
+          <div>
+            <button
+              className="rounded-xl w-full shadow-2xl inline-flex items-center h-12 px-5 text-indigo-100 transition duration-200 ease-in-out bg-blue-600 hover:bg-black transform hover:-translate-y-1 hover:scale-110 .."
+              onClick={() => onClickHandler('github')}
+            >
+              <span>Login with Github</span>
+              <svg
+                className="w-5 h-5 ml-2  fill-current icon"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
+      <br />
       <div className="custom-card max-w-md mx-auto rounded-xl shadow-2xl overflow-hidden md:max-w-2xl">
         <div className="sm:flex">
           <div className="p-8">
@@ -171,7 +186,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-
       <br />
     </div>
   );
