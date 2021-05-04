f641381b5be9686124bf3ff4f5ab4c8e0e4703a8
JaganKaartik <kaartikjagan@gmail.com>
2021-05-04 21:40:39 +0530

    Code-Splitting Components

--- app/src/Router/index.js
+++ app/src/Router/index.js
@@ -1,14 +1,11 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { DataProvider } from '../context/DataContext';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import {
-  Home,
-  Onboarding,
-  Dashboard,
-  Navbar,
-  AuthComponent,
-  AnalyticsDashboard,
-} from '../components';
+import { Home, Navbar, AuthComponent } from '../components';
+
+const { Dashboard, Onboarding, AnalyticsDashboard } = React.lazy(() =>
+  import('../components')
+);
 
 export default function Router() {
   return (
@@ -19,23 +16,26 @@ export default function Router() {
           <Route exact path="/app/home">
             <Home />
           </Route>
-          <Route exact path="/app/onboarding">
-            <AuthComponent>
-              <Onboarding />
-            </AuthComponent>
-          </Route>
-          <DataProvider>
-            <Route exact path="/app/dashboard">
+
+          <Suspense fallback={<p>Loading</p>}>
+            <Route exact path="/app/onboarding">
               <AuthComponent>
-                <Dashboard />
+                <Onboarding />
               </AuthComponent>
             </Route>
-            <Route exact path="/app/analytics">
-              <AuthComponent>
-                <AnalyticsDashboard />
-              </AuthComponent>
-            </Route>
-          </DataProvider>
+            <DataProvider>
+              <Route exact path="/app/dashboard">
+                <AuthComponent>
+                  <Dashboard />
+                </AuthComponent>
+              </Route>
+              <Route exact path="/app/analytics">
+                <AuthComponent>
+                  <AnalyticsDashboard />
+                </AuthComponent>
+              </Route>
+            </DataProvider>
+          </Suspense>
         </div>
       </Switch>
     </BrowserRouter>
