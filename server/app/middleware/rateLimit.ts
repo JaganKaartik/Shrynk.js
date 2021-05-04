62da0477a75141025941277f3b7d0d3cb440de71
JugHead! <kaartikjagan@gmail.com>
2021-05-05 00:05:50 +0530

    Update and rename RateLimit.ts to rateLimit.ts

--- server/app/middleware/rateLimit.ts
+++ server/app/middleware/rateLimit.ts
@@ -2,9 +2,9 @@ const rateLimit = require('express-rate-limit')
 
 const apiLimiter = rateLimit({
   windowMs: 1 * 60 * 1000,
-  max: 50,
+  max: 30,
   message:
-    'Too many accounts created from this IP, please try again after a minute'
+    'Too many api-hits created from this IP, please try again after a minute'
 })
 
 export = apiLimiter
