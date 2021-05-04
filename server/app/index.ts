559a6e99b434b07008ae67f1aa728cf63434ecef
JaganKaartik <kaartikjagan@gmail.com>
2021-05-05 00:44:03 +0530

    Prod Issue -- Fix

--- server/app/index.ts
+++ server/app/index.ts
@@ -9,19 +9,18 @@ const { PORT, NODE_ENV } = require('./config/default.config')
 const port = PORT || 8000
 
 const app = express()
+app.use(favicon(path.join(__dirname, '../../app/public/favicon.ico')))
+
+connectDB()
+app.use(Middleware)
 
 if (NODE_ENV === 'production') {
-  app.use(favicon(path.join(__dirname, '../../app/public/favicon.ico')))
   app.disable('x-powered-by')
   app.use(express.static(path.join(__dirname, '../../app/build')))
   app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../app/build', 'index.html'))
   })
 }
-
-connectDB()
-
-app.use(Middleware)
 
 app.listen(port, () => {
   console.log(`App running at port: ${port}`)
