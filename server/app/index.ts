cec2835e9d3b8980a47231dfc6013a8c9a8d6d32
Jagan Kaartik <kaartikjagan@gmail.com>
2021-06-02 16:16:30 +0530

    Minor.

--- server/app/index.ts
+++ server/app/index.ts
@@ -10,12 +10,12 @@ const port = PORT || 8000
 
 const app = express()
 app.use(favicon(path.join(__dirname, '../../app/public/favicon.ico')))
+app.disable('x-powered-by')
 
 connectDB()
 app.use(Middleware)
 
 if (NODE_ENV === 'production') {
-  app.disable('x-powered-by')
   app.use(express.static(path.join(__dirname, '../../app/build')))
   app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../app/build', 'index.html'))
